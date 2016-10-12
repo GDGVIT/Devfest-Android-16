c3df37d086473267314e832649743c1e1783a050
abhinav-adtechs <abhinav.adtechs@gmail.com>
2016-10-12 08:49:51 +0530

    item click removed and button enabled

--- app/src/main/java/com/gdgvitvellore/devfest/Control/ViewHolder/FAQQuestionViewHolder.java
+++ app/src/main/java/com/gdgvitvellore/devfest/Control/ViewHolder/FAQQuestionViewHolder.java
@@ -2,6 +2,7 @@ package com.gdgvitvellore.devfest.Control.ViewHolder;
 
 
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bignerdranch.expandablerecyclerview.ViewHolder.ParentViewHolder;
@@ -11,15 +12,30 @@ import com.gdgvitvellore.devfest.gdgdevfest.R;
 public class FAQQuestionViewHolder extends ParentViewHolder{
 
     private TextView tvQuestion ;
+    private ImageView ivDown ;
 
     public FAQQuestionViewHolder(View itemView) {
         super(itemView);
         tvQuestion = (TextView) itemView.findViewById(R.id.card_list_parent_question_title) ;
+        ivDown = (ImageView) itemView.findViewById(R.id.card_list_parent_question_answer) ;
+        ivDown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isExpanded()) {
+                    collapseView();
+                } else {
+                    expandView();
+                }
+            }
+        });
     }
 
     public void bind(FAQ faqEntity) {
         tvQuestion.setText(faqEntity.getQuestion());
     }
 
-
+    @Override
+    public boolean shouldItemViewClickToggleExpansion() {
+        return false ;
+    }
 }
